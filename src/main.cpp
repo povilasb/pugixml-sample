03e647b301dd7f602a3c9d8ed52c11406fcd2838
Povilas Balciunas <balciunas90@gmail.com>
2014-07-25 20:23:34 +0300

    Updates sample code.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ namespace xml = pugi;
 const string str_xml =
 "<html>"
 	"<body>"
+		"<p>Hello world</p>"
 	"</body>"
 "</html>"
 ;
@@ -21,7 +22,8 @@ main(void)
 {
 	xml::xml_document doc;
 	doc.load(str_xml.c_str());
-	cout << doc.child("body") << '\n';
+	cout << doc.child("html").child("body").child("p").first_child()
+		.value() << '\n';
 
 	return 0;
 }
